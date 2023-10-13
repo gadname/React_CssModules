d943fa6c7187b4777884d6fef32e92769b242e26
GAKU <gakugaku1114@iCloud.com>
2023-10-13 12:54:17 +0000

    Update Emotion.jsx

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,7 +1,21 @@
-import { jsx } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
 export const Emotion = () => {
+  const containerStyle = css`
+    border: solid 2px #392eff;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  `;
+  const titlestyle = css({
+    margin: 0,
+    color: "#3d84a8",
+  });
   return (
-    <div>
+    <div css={containerStyle}>
       <p>Emotion</p>
       <button>FIGHT!</button>
     </div>
