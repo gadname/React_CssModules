f8b35f93e6cb34ea6cf8379c5d239a5430e516dd
GAKU <gakugaku1114@iCloud.com>
2023-10-13 08:38:40 +0000

    Update 5 files

---
+++ src/components/StyledComponents.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+export const StyledComponents = () => {
+  return (
+    <Container>
+      <p>Styled Components</p>
+      <button>FIGHT!</button>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  border: solid 2px #392eff;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
